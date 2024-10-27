65e0f37095e653a55b6854f1a7a5d39abafcac1e
lorondo <mralexanderlindsay@gmail.com>
2024-10-27 16:02:41 -0700

    added database connection

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const Users = Models.User;
 
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/cfDB',
+// mongoose.connect('mongodb://localhost:27017/cfDB',
+//     { 
+//       useNewUrlParser: true, 
+//       useUnifiedTopology: true
+//     });
+
+mongoose.connect( process.env.CONNECTION_URI,
     { 
       useNewUrlParser: true, 
       useUnifiedTopology: true
